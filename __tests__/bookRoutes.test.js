1d3b36e051c4e2de13a1f84a41d1d73c023cd8f8
zataara <bryantmac@gmail.com>
2021-10-08 11:55:20 -0700

    added test for POSTing new book to db

--- __tests__/bookRoutes.test.js
+++ __tests__/bookRoutes.test.js
@@ -25,4 +25,29 @@ beforeEach(async () => {
     book_isbn = result.rows[0].isbn
 });
 
+describe("POST /books", async function () {
+    test("Creates a new book", async function () {
+        const response = await request(app)
+            .post(`/books`)
+            .send({
+                isbn: '1894542',
+                amazon_url: "http;//www.books.com",
+                author: "test_author",
+                language: "English",
+                pages: 250,
+                publisher: "SomethingClearingHouse",
+                title: "The book of the year",
+                year: 2020
+            });
+        expect(response.statusCode).toBe(201);
+        expect(response.body.book).toHaveProperty("isbn");
+    });
+
+    test("Prevents creating book without required title", async function() {
+        const response = await request(app)
+            .post(`/books`)
+            .send({year: 2000});
+        expect(response.statusCode).toBe(400);
+    });
+});
 
